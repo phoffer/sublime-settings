c59f54f915040ee762be3bfd476459dca69f06f2
Paul Hoffer <paulrhoffer@gmail.com>
2014-01-27 10:42:32 -0700

    init

---
+++ syntax-test-ruby.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+
+Constant = :something
+class Klass
+  Consta = :hello
+  def something
+    #etc
+  end
+  class << self
+    # stuff
+  end
+  # stuff
+end
+class Inh < Klass
+  # stuff
+end
